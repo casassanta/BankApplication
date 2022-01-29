d07ebfcebbfe13c8e87c0b2aba3ef69e4bf903dd
Guilherme Casassanta <guilherme.casassanta@gmail.com>
2022-01-29 16:12:04 -0300

    Fixed address validations and added date validation

--- src/main/kotlin/com/gcc/bankapplication/controller/response/CustomerResponse.kt
+++ src/main/kotlin/com/gcc/bankapplication/controller/response/CustomerResponse.kt
@@ -1,14 +1,14 @@
 package com.gcc.bankapplication.controller.response
 
-import com.gcc.bankapplication.controller.request.PostDocumentRequest
 import com.gcc.bankapplication.model.enums.Nationalities
+import java.time.LocalDate
 import java.util.*
 
 data class CustomerResponse(
     val id: UUID,
     val firstName: String,
     val lastName: String,
-    val birthDate: Date,
+    val birthDate: LocalDate,
     val nationality: Nationalities,
     val document: DocumentResponse,
     val addresses: List<AddressResponse>
